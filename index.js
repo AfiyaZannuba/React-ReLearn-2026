91c7e61a1fab0ed23bf1ff11638e3f551e129711
AfiyaZannuba <afiya.zannubaa@gmail.com>
2026-08-02 00:01:59 +0400

    Setting up React using CDNs without any Vite like webpacks

---
+++ index.js
@@ -0,0 +1,8 @@
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<ul>
+  <li>I am learning React</li>
+  <li>I want to build websites</li>
+  <li>I want to learn react native</li>
+  <li>I want to build apps from scratch</li>
+  <li>I want to earn from selling apps</li>
+</ul>)
--- index.jsx
+++
@@ -1,4 +0,0 @@
-import { createRoot } from "react-dom/client"
-
-// Create a root
-//Render some markup to the root
